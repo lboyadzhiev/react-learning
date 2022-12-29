0c6b074405921f61cab40aaf1af3942317847c05
lboyadzhiev <l.boyadzhievv@abv.bg>
2022-12-29 14:32:59 +0200

    multiple components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+    const expenses = [
+        {
+            title: 'Grajdanska otgovornost',
+            amount: 294.67,
+            date: new Date(2022, 12, 17),
+        },
+        {
+            title: 'Vinetka',
+            amount: 97.0,
+            date: new Date(2022, 12, 17),
+        },
+        {
+            title: 'Techicneski pregled',
+            amount: 50.0,
+            date: new Date(2022, 12, 17),
+        },
+    ];
     return (
         <div>
             <h2>Let's get started!</h2>
-            <ExpenseItem></ExpenseItem>
+            {expenses.map((item) => {
+                return (
+                    <ExpenseItem
+                        title={item.title}
+                        amount={item.amount}
+                        date={item.date}
+                    ></ExpenseItem>
+                );
+            })}
         </div>
     );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,17 @@
 // style
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 12, 17);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
+// components
+import ExpenseDate from './ExpenseDate';
 
+function ExpenseItem({ title, amount, date }) {
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
+            <ExpenseDate date={date} />
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
+                <h2>{title}</h2>
             </div>
-            <div className='expense-item__price'>${expenseAmount}</div>
+            <div className='expense-item__price'>${amount}</div>
         </div>
     );
 }
